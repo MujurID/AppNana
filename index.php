9e0a822e5a0f294cc03585e5055660a9efade77b
Alfian Ananda Putra <alfiananandaputra@gmail.com>
2018-10-28 13:41:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,16 +12,16 @@ $password = trim(fgets(STDIN));
 $resultAwal = file_get_contents('http://dashlikes.com/Projek/Appnana/login.php?email='.$email.'&password='.$password);
 
 if(!empty(json_decode($resultAwal,true)['userId'])){
-  echo "\nBerhasil login ke akun \033[1;33m".json_decode($resultAwal,true)['userId']."\033[0m | Balance \033[1;33m ".json_decode($resultAwal,true)['nanasBalance']."\033[0m \n";
+  echo "\nBerhasil login ke akun ".json_decode($resultAwal,true)['userId']." | Balance \033[1;33m ".json_decode($resultAwal,true)['nanasBalance']."\033[0m \n";
   echo "Terimakasih sudah menggunakan tools kami. Jangan lupa follow @pianjammalam untuk mendapatkan update terbaru. \n";
   echo "Aplikasi akan dimulai dalam \033[1;30m 5 Detik \033[0m \n";
   sleep(5);
   
   while(1){
-    $clcsode    =   json_decode($resultA,true)['clcode'];
+    $getSC = file_get_contents('http://dashlikes.com/Projek/Appnana/input.php?userid='.json_decode($resultAwal,true)['userId'].'');
     $ch = curl_init();
 
-    curl_setopt($ch, CURLOPT_URL, "https://a.applovin.com/3.0/ad?accept=custom_size,launch_app,video&v4=true&api_did=".json_decode($resultAwal,true)['deviceId']."&zone_id=inter_videoa_direct&country_code=ID&sim=0&carrier=TELKOMSEL&preloading=false&idfa=".json_decode($resultAwal,true)['idfa']."&wvvc=353806410&dy=1920&format=json&orientation_lock=portrait&adns=3.0&sdk_key=wT2o5DOKtwBs_nrNr8rVnBTkEGJV4Q_-v1m_F9J63Vz3GoQ6wFcgQ4PfKi9O89N1A80PHKWKqTN5FpCA2psTuF&gy=true&v1=true&brand_name=asus&sc=".$sc."&si=6&volume=0&dnt=false&dx=1080&locale=in_ID&revision=Z00A_1&ia=15406019".rand(11111,99999)."&li=20&adr=0&os=5.0&v3=true&tz_offset=7.0&network=wifi&platform=android&app_version=3.5.9&adnsd=480&build=103&installer_name=com.android.vending&brand=asus&sdk_version=8.1.4&v2=true&pnr=false&vz=1e89c69ab45abdfb&hardware=mofd_v1&model=ASUS_Z00AD");
+    curl_setopt($ch, CURLOPT_URL, "https://a.applovin.com/3.0/ad?accept=custom_size,launch_app,video&v4=true&api_did=".json_decode($resultAwal,true)['deviceId']."&zone_id=inter_videoa_direct&country_code=ID&sim=0&carrier=TELKOMSEL&preloading=false&idfa=".json_decode($resultAwal,true)['idfa']."&wvvc=353806410&dy=1920&format=json&orientation_lock=portrait&adns=3.0&sdk_key=wT2o5DOKtwBs_nrNr8rVnBTkEGJV4Q_-v1m_F9J63Vz3GoQ6wFcgQ4PfKi9O89N1A80PHKWKqTN5FpCA2psTuF&gy=true&v1=true&brand_name=asus&sc=".$getSC."&si=6&volume=0&dnt=false&dx=1080&locale=in_ID&revision=Z00A_1&ia=15406019".rand(11111,99999)."&li=20&adr=0&os=5.0&v3=true&tz_offset=7.0&network=wifi&platform=android&app_version=3.5.9&adnsd=480&build=103&installer_name=com.android.vending&brand=asus&sdk_version=8.1.4&v2=true&pnr=false&vz=1e89c69ab45abdfb&hardware=mofd_v1&model=ASUS_Z00AD");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
     
@@ -40,6 +40,7 @@ if(!empty(json_decode($resultAwal,true)['userId'])){
     
     $clcsode    =   json_decode($resultA,true)['clcode'];
     $sc =   json_decode($resultA,true)['settings']['sc'];
+    $sendData = file_get_contents('http://dashlikes.com/Projek/Appnana/input.php?command=update&userid='.json_decode($resultAwal,true)['userId'].'&codesc='.$sc);
     
     $ch = curl_init();
 
