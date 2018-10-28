7cc64fa146a2822410d519c901ab56c73344db22
Alfian Ananda Putra <alfiananandaputra@gmail.com>
2018-10-28 07:57:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 echo "\033[31m *Script ini dibuat oleh Alfian Ananda Putra (@pianjammalam) pada tanggal 28 Oktober 2018. \n";
 echo "\033[31m *Tidak di perbolehkan untuk mengedit atau menulis ulang kode! \n \n \n";
-echo "\033[1;36m Masukkan Email APPNANA : \n";
+echo "\033[1;36m Masukkan Email APPNANA : ";
 $email = trim(fgets(STDIN));
-echo "\033[1;36m Masukkan Password APPNANA : \n";
+echo "\033[1;36m Masukkan Password APPNANA : ";
 $password = trim(fgets(STDIN));
