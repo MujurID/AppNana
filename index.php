ef9150220b3d3345d8cfefa5f5015ed7536c18ee
Alfian Ananda Putra <alfiananandaputra@gmail.com>
2018-10-28 14:16:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ if(!empty(json_decode($resultAwal,true)['userId'])){
         }
         curl_close ($ch);
         
-        echo "\033[1;33m".json_decode($resultAwal,true)['userId'].">>".$sccode.">>".print_r($resultA)."||".json_decode($result,true)['results'][0]['result']."\033[0m \n";
+        echo "\033[1;33m".json_decode($resultAwal,true)['userId'].">>".$sccode.">>".print_r($resultA).">>"."https://a.applovin.com/3.0/ad?accept=custom_size,launch_app,video&v4=true&api_did=".json_decode($resultAwal,true)['deviceId']."&zone_id=inter_videoa_direct&country_code=ID&sim=0&carrier=TELKOMSEL&preloading=false&idfa=".json_decode($resultAwal,true)['idfa']."&wvvc=353806410&dy=1920&format=json&orientation_lock=portrait&adns=3.0&sdk_key=wT2o5DOKtwBs_nrNr8rVnBTkEGJV4Q_-v1m_F9J63Vz3GoQ6wFcgQ4PfKi9O89N1A80PHKWKqTN5FpCA2psTuF&gy=true&v1=true&brand_name=asus&sc=".$sccode."&si=6&volume=0&dnt=false&dx=1080&locale=in_ID&revision=Z00A_1&ia=15406019".rand(11111,99999)."&li=20&adr=0&os=5.0&v3=true&tz_offset=7.0&network=wifi&platform=android&app_version=3.5.9&adnsd=480&build=103&installer_name=com.android.vending&brand=asus&sdk_version=8.1.4&v2=true&pnr=false&vz=1e89c69ab45abdfb&hardware=mofd_v1&model=ASUS_Z00AD"."||".json_decode($result,true)['results'][0]['result']."\033[0m \n";
 
     }
     
