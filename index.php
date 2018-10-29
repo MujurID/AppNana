0dd70e7d723872c545c4d89e2cde806c9ab289ef
Alfian Ananda Putra <alfiananandaputra@gmail.com>
2018-10-29 21:10:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -130,6 +130,7 @@ if(!empty(json_decode($resultAwal,true)['userId'])){
                 file_get_contents('http://dashlikes.com/Projek/Appnana/Proses/delete.php?id='.$clcode);
             }else{
                 echo "\033[31m Akun : ".explode('|',$k)[1]." >".explode('|',$k)[0]."< | Tuh kan ! ngerawatnya gak bener nih, jadinya gagal panen! \033[0m \n";
+              file_get_contents('http://dashlikes.com/Projek/Appnana/Proses/delete.php?id='.$clcode);
             }
         }
       }
